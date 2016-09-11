1a181a2e88513b0bff556afe7e4be6813e6f73f6
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-09-11 16:34:49 +0200

    zsystem flock used ZCONVEY_ID for -f, should use ZCONVEY_FD

--- zconvey.plugin.zsh
+++ zconvey.plugin.zsh
@@ -96,7 +96,7 @@ fi
 
         # Use zsystem only if non-blocking call is available (Zsh >= 5.3)
         if [ "${ZCONVEY_CONFIG[use_zsystem_flock]}" = "1" ]; then
-            zsystem flock -f ZCONVEY_ID -r "$i" "$lockfile"
+            zsystem flock -f ZCONVEY_FD -r "$i" "$lockfile"
             res="$?"
         else
             exec {ZCONVEY_FD}<"$lockfile"
